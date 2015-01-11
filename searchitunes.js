e52bfe0cc325711e463bf95a171e82c985aa9efd
Franklin van de Meent <fr@nkl.in>
2015-01-11 17:05:04 +0100

    Rewrite request, error handling, callback change
    * New callback: `function( err, res ) {}`

--- searchitunes.js
+++ searchitunes.js
@@ -13,43 +13,74 @@ var http = require('http')
 var querystring = require('querystring')
 
 module.exports = function( params, cb ) {
+  // prevent multiple callbacks
+  var complete = false
+  function doCallback( err, res ) {
+    if( !complete ) {
+      complete = true
+      cb( err, res || null )
+    }
+  }
+
+  // build request
   params.version = params.version || 2
 
-  var req = http.request(
-    {
-      host: 'itunes.apple.com',
-      path: '/WebObjects/MZStoreServices.woa/ws/wsSearch?'+ querystring.stringify( params ),
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'User-Agent': 'searchitunes.js'
-      }
-    },
-    function( response ) {
-      var data = ''
-      response.on( 'data', function( chunk ) { data += chunk })
-      response.on( 'end', function() {
-
-        // process response
-        data = data.toString().trim()
-        if( data.length >= 2 && data.substr(0,1) == '{' && data.substr( data.length -1, 1 ) == '}' ) {
-
-          data = JSON.parse( data )
-          if( data.resultCount !== undefined ) {
-            cb( data )
-          } else {
-            cb( false, {reason: 'incomplete json', headers: response.headers, data: data} )
-          }
-
-        } else {
-
-          // not json
-          cb( false, {reason: 'not json', headers: response.headers, data: data} )
-
-        }
-
-      })
-
+  var options = {
+    host: 'itunes.apple.com',
+    path: '/WebObjects/MZStoreServices.woa/ws/wsSearch?'+ querystring.stringify( params ),
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'User-Agent': 'searchitunes.js'
     }
-  ).end()
+  }
+
+  var request = http.request( options )
+
+  // response
+  request.on( 'response', function( response ) {
+    var data = []
+    var size = 0
+    
+    response.on( 'data', function( ch ) {
+      data.push( ch )
+      size += ch.length
+    })
+
+    response.on( 'close', function() {
+      doCallback( new Error('request closed') )
+    })
+    
+    response.on( 'end', function() {
+      data = new Buffer.concat( data, size ).toString('utf8').trim()
+
+      if( response.statusCode >= 300 ) {
+        var error = new Error('http error')
+        error.code = response.statusCode
+        error.body = data
+        doCallback( error )
+        return
+      }
+
+      try {
+        data = JSON.parse( data )
+        doCallback( null, data )
+      }
+      catch(e) {
+        var error = new Error('invalid response')
+        error.error = e
+        doCallback( error )
+      }
+    })
+  })
+
+  // request failed
+  request.on( 'error', function( err ) {
+    var error = new Error('request failed')
+    error.error = err
+    doCallback( error )
+  })
+
+  // run it
+  request.end()
 }
