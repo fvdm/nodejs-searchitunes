a268bbe20c9bd6d1c1476476621fae9c0119c4d8
Franklin van de Meent <fr@nkl.in>
2015-01-11 16:33:11 +0100

    Clean up
    * replaced tabs with two spaces
    * strip trailing whitespace
    * added var's
    * minor clean up

--- searchitunes.js
+++ searchitunes.js
@@ -10,54 +10,47 @@ License:      Unlicense / Public Domain, see UNLICENSE file
 API docs:     http://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html
 */
 
-var	http = require('http'),
-	querystring = require('querystring'),
-	api = {
-		host:	'itunes.apple.com',
-		port:	80,
-		path:	'/WebObjects/MZStoreServices.woa/ws/wsSearch'
-	}
+var http = require('http')
+var querystring = require('querystring')
 
 module.exports = function( params, cb ) {
-	params.version = params.version || 2
-	
-	var req = http.request(
-		{
-			host:		api.host,
-			port:		api.port,
-			path:		api.path +'?'+ querystring.stringify( params ),
-			method:		'GET',
-			headers: {
-				'Accept':	'application/json',
-				'User-Agent':	'appstoresearch.js'
-			}
-		},
-		function( response ) {
-			
-			var data = ''
-			response.on( 'data', function( chunk ) { data += chunk })
-			response.on( 'end', function() {
-				
-				// process response
-				data = data.toString().trim()
-				if( data.length >= 2 && data.substr(0,1) == '{' && data.substr( data.length -1, 1 ) == '}' ) {
-					
-					data = JSON.parse( data )
-					if( data.resultCount !== undefined ) {
-						cb( data )
-					} else {
-						cb( false, {reason: 'incomplete json', headers: response.headers, data: data} )
-					}
-					
-				} else {
-					
-					// not json
-					cb( false, {reason: 'not json', headers: response.headers, data: data} )
-					
-				}
-			
-			})
-		
-		}
-	).end()
+  params.version = params.version || 2
+
+  var req = http.request(
+    {
+      host: 'itunes.apple.com',
+      path: '/WebObjects/MZStoreServices.woa/ws/wsSearch?'+ querystring.stringify( params ),
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'searchitunes.js'
+      }
+    },
+    function( response ) {
+      var data = ''
+      response.on( 'data', function( chunk ) { data += chunk })
+      response.on( 'end', function() {
+
+        // process response
+        data = data.toString().trim()
+        if( data.length >= 2 && data.substr(0,1) == '{' && data.substr( data.length -1, 1 ) == '}' ) {
+
+          data = JSON.parse( data )
+          if( data.resultCount !== undefined ) {
+            cb( data )
+          } else {
+            cb( false, {reason: 'incomplete json', headers: response.headers, data: data} )
+          }
+
+        } else {
+
+          // not json
+          cb( false, {reason: 'not json', headers: response.headers, data: data} )
+
+        }
+
+      })
+
+    }
+  ).end()
 }
