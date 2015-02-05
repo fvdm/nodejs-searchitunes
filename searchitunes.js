b55049b956c283ff95e1ffbb52d4b98477448388
Franklin van de Meent <fr@nkl.in>
2015-02-05 12:34:32 +0100

    Clean up code style (fixed #2)

--- searchitunes.js
+++ searchitunes.js
@@ -9,89 +9,89 @@ License:      Unlicense / Public Domain, see UNLICENSE file
               (https://github.com/fvdm/nodejs-searchitunes/raw/master/UNLICENSE)
 */
 
-var http = require('http')
-var querystring = require('querystring')
+var http = require ('http');
+var querystring = require ('querystring');
 
-module.exports = function( params, cb ) {
+module.exports = function (params, cb) {
   // prevent multiple callbacks
-  var complete = false
-  function doCallback( err, res ) {
-    if( !complete ) {
-      complete = true
-      cb( err, res || null )
+  var complete = false;
+  function doCallback (err, res) {
+    if (!complete) {
+      complete = true;
+      cb (err, res || null);
     }
   }
 
   // check input
-  if( !params || !(params instanceof Object) ) {
-    doCallback( new Error('invalid params') )
-    return
+  if (!params || !(params instanceof Object)) {
+    doCallback (new Error('invalid params'));
+    return;
   }
 
   // build request
-  params.version = params.version || 2
+  params.version = params.version || 2;
 
   var options = {
     host: 'itunes.apple.com',
-    path: '/WebObjects/MZStoreServices.woa/ws/wsSearch?'+ querystring.stringify( params ),
+    path: '/WebObjects/MZStoreServices.woa/ws/wsSearch?'+ querystring.stringify (params),
     method: 'GET',
     headers: {
       'Accept': 'application/json',
       'User-Agent': 'searchitunes.js'
     }
-  }
+  };
 
-  var request = http.request( options )
+  var request = http.request (options);
 
   // response
-  request.on( 'response', function( response ) {
-    var data = []
-    var size = 0
+  request.on ('response', function (response) {
+    var data = [];
+    var size = 0;
     
-    response.on( 'data', function( ch ) {
-      data.push( ch )
-      size += ch.length
-    })
+    response.on ('data', function (ch) {
+      data.push(ch);
+      size += ch.length;
+    });
 
-    response.on( 'close', function() {
-      doCallback( new Error('request closed') )
-    })
+    response.on ('close', function () {
+      doCallback (new Error ('request closed'));
+    });
     
-    response.on( 'end', function() {
-      data = new Buffer.concat( data, size ).toString('utf8').trim()
+    response.on ('end', function () {
+      data = new Buffer.concat (data, size).toString ('utf8').trim ();
 
       if( response.statusCode >= 300 ) {
-        var error = new Error('http error')
-        error.code = response.statusCode
-        error.body = data
-        doCallback( error )
-        return
+        var error = new Error ('http error');
+        error.code = response.statusCode;
+        error.body = data;
+        doCallback (error);
+        return;
       }
 
       try {
-        data = JSON.parse( data )
+        data = JSON.parse (data);
 
-        if( !(data.results instanceof Array) || data.results.length === 0 ) {
-          doCallback( new Error('no results') )
+        if (!(data.results instanceof Array) || data.results.length === 0) {
+          doCallback (new Error('no results'));
         } else {
-          doCallback( null, data )
+          doCallback (null, data);
         }
       }
-      catch(e) {
-        var error = new Error('invalid response')
-        error.error = e
-        doCallback( error )
+      catch (e) {
+        var error = new Error ('invalid response');
+        error.error = e;
+        doCallback (error);
       }
-    })
-  })
+    });
+  });
 
   // request failed
-  request.on( 'error', function( err ) {
-    var error = new Error('request failed')
-    error.error = err
-    doCallback( error )
-  })
+  request.on ('error', function (err) {
+    var error = new Error ('request failed');
+    error.error = err;
+    doCallback (error);
+  });
 
   // run it
-  request.end()
+  request.end ();
 }
