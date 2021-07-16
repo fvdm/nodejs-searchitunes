81346e49d94c14e9b8dbdcafb994aa7847964917
Franklin <fr@nkl.in>
2021-07-17 01:28:06 +0200

    Chore(example): Fixed parameters argument

--- example.js
+++ example.js
@@ -1,17 +1,15 @@
 // Load module
 const itunes = require ('searchitunes');
 
-// Search parameters
-const parameters = {
+// Find an app
+itunes ({
   entity: 'software',
   country: 'NL',
   term: 'github',
   limit: 1,
   price: 0,
-};
 
-// Do the search
-itunes ({ parameters })
+})
   .then (data => console.dir (data, {
     depth: null,
     colors: true,
